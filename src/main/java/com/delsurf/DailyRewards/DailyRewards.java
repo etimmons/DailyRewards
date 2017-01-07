633e3ed6e7a91fbc2ce3cc31aa9d26c512736a14
Ed Timmons <ed@4cinc.com>
2017-01-07 16:24:01 -0500

    Initial functionality - Run the command, answer a question, get a reward

---
+++ src/main/java/com/delsurf/DailyRewards/DailyRewards.java
@@ -0,0 +1,69 @@
+package com.delsurf.DailyRewards;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DailyRewards extends JavaPlugin {
+	@Override
+    public void onEnable() {
+		getLogger().info("onEnable has been invoked!");
+    }
+    
+    @Override
+    public void onDisable() {
+    	getLogger().info("onDisable has been invoked!");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("dr")) {
+    		if (sender instanceof Player) {
+    			
+                Player player = (Player) sender;
+                
+                getLogger().info("onCommand has been invoked by " + player.getDisplayName() + "!");
+                
+                if (args.length == 0) {
+                	sender.sendMessage("What is the square root of 100?");
+                	return false;
+                }
+                
+                String answer = args[0];
+                
+                boolean isAnswerCorrect = this.isAnswerCorrect(answer);
+                
+	            if (isAnswerCorrect == true) {
+	                String msg = "'" + answer + "' is Correct! You have earned 1 Diamond!";
+	                player.sendMessage(msg);
+	                PlayerInventory inventory = player.getInventory(); // The player's inventory
+	                ItemStack itemstack = new ItemStack(Material.DIAMOND, 1); // A stack of diamonds (or not)
+	                inventory.addItem(itemstack); // Adds the stack to the player's inventory
+                } else {                	
+                	sender.sendMessage("'" + answer + "' is Incorrect.");
+                }
+            } else {
+                sender.sendMessage("You must be a player!");
+                return false;
+            }
+    		return true;
+    	}
+    	
+        // do something
+        return false;
+    }
+    
+    public boolean isAnswerCorrect(String answer) {
+    	String expectedAnswer = "100";
+    	
+    	if (answer.equals(expectedAnswer)) {
+    		return true;
+    	}
+    	
+    	return false;
+    }
+}
